33ed335ce67d409f202b0645468e90169da989ae
Maytham Mahdi <51288320+maytham553@users.noreply.github.com>
2024-05-27 11:36:51 +0300

    optmize migration file

--- database/migrations/2024_05_25_110157_add_ldap_columns_to_users_table.php
+++ database/migrations/2024_05_25_110157_add_ldap_columns_to_users_table.php
@@ -19,46 +19,6 @@ class AddLdapColumnsToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) use ($driver) {
             $table->string('guid')->nullable();
             $table->string('domain')->nullable();
-
-            if ($driver !== 'sqlsrv') {
-                $table->unique('guid');
-            }
         });
-
-        if ($driver === 'sqlsrv') {
-            DB::statement(
-                $this->compileUniqueSqlServerIndexStatement('users', 'guid')
-            );
-        }
-    }
-
-    /**
-     * Reverse the migrations.
-     * 
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['guid', 'domain']);
-        });
-    }
-
-    /**
-     * Compile a compatible "unique" SQL Server index constraint.
-     * 
-     * @param string $table
-     * @param string $column 
-     * 
-     * @return string 
-     */
-    protected function compileUniqueSqlServerIndexStatement($table, $column)
-    {
-        return sprintf('create unique index %s on %s (%s) where %s is not null',
-            implode('_', [$table, $column, 'unique']),
-            $table,
-            $column,
-            $column
-        );
     }
 }
